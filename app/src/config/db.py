d6a257beb3aea993e09ae9d3c59d565f14ffb793
Shivam Rathore <7shivam.z@gmail.com>
2022-04-22 17:02:26 +0530

    add env variables in db file

--- app/src/config/db.py
+++ app/src/config/db.py
@@ -1,5 +1,11 @@
 from sqlalchemy import create_engine, MetaData
+import os
 
-engine = create_engine('mysql+pymysql://root:root@localhost:3306/s3_moviemasala_db')
+ipaddress = os.environ.get('IP_ADDRESS')
+mysql_host_name = os.environ.get('HOST_NAME')
+mysql_host_pass = os.environ.get('HOST_PASS')
+database_name = os.environ.get('DATABASE')
+
+engine = create_engine(f'mysql+pymysql://{mysql_host_name}:{mysql_host_pass}@{ipaddress}:3306/{database_name}')
 meta = MetaData()
 conn = engine.connect()

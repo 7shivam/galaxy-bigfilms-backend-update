612d4b88c40736036113a2ae6d6b30e7f05b29e3
Shivaraj600 <69134270+Shivaraj600@users.noreply.github.com>
2022-04-26 16:10:31 +0530

    Update db.py

--- app/src/config/db.py
+++ app/src/config/db.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine, MetaData
 import os
 
 ipaddress = os.environ.get('IP_ADDRESS')
-mysql_host_name = os.environ.get('HOST_NAME')
-mysql_host_pass = os.environ.get('HOST_PASS')
-database_name = os.environ.get('DATABASE')
+mysql_host_name = os.environ.get(''USER_NAME')
+mysql_host_pass = os.environ.get('MYSQL_ROOT_PASSWORD')
+database_name = os.environ.get('MYSQL_DATABASE')
 
 engine = create_engine(f'mysql+pymysql://{mysql_host_name}:{mysql_host_pass}@{ipaddress}:3306/{database_name}')
 meta = MetaData()
